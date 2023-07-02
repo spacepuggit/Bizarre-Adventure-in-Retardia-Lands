0a9f453f0adb05b19d5d6f0206903fd7accc2ec4
aabramov <aabramov@gear-games.com>
2023-07-02 20:47:56 +0300

    remove map

--- Assets/Scripts/CoreBAIRL/SceneData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewSceneData", menuName = "Scene Data", order = 51)]
-public class SceneData : ScriptableObject
-{
-    public string sceneName;
-    public string description;
-    public Sprite sceneImage;
-}
