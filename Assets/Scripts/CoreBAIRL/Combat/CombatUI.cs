78e8529c57359254c100205461a8e93e785072d3
aabramov <aabramov@gear-games.com>
2023-07-04 13:53:26 +0300

    update ui

--- Assets/Scripts/CoreBAIRL/Combat/CombatUI.cs
+++ Assets/Scripts/CoreBAIRL/Combat/CombatUI.cs
@@ -1,137 +1,34 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CombatUI : MonoBehaviour
 {
-    public TMP_Dropdown bodyPartDropdown;
-    public Button attackButton;
-    public Button defendButton;
-    private CombatSystem combatSystem;
-    private int selectedBodyPartIndex = 0;
-    private bool isChoosingAction = true; // Add this
-    private bool isAttacking = false; // Add this
+    private VisualElement rootElement;
+    private Button attackButton;
+    private Button defendButton;
 
     private void Start()
     {
-        combatSystem = ServiceLocator.Instance.Get<CombatSystem>();
-        PopulateBodyPartDropdown();
-        attackButton.onClick.AddListener(OnAttackButton);
-        defendButton.onClick.AddListener(OnDefendButton);
-        combatSystem.OnTurnChanged += OnTurnChanged;
+        // Get a reference to the root UI document
+        var uiDocument = GetComponent<UIDocument>();
+        rootElement = uiDocument.rootVisualElement;
+
+        // Find the UI elements
+        attackButton = rootElement.Q<Button>("AttackButton");
+        defendButton = rootElement.Q<Button>("DefendButton");
+
+        // Add event listeners
+        attackButton.clicked += OnAttackButtonClicked;
+        defendButton.clicked += OnDefendButtonClicked;
     }
 
-    private void Update()
+    private void OnAttackButtonClicked()
     {
-        if (combatSystem.isPlayerTurn)
-        {
-            if (isChoosingAction)
-            {
-                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-                {
-                    isAttacking = !isAttacking;
-                    // Update the UI to reflect the new selection
-                }
-
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    isChoosingAction = false;
-                    bodyPartDropdown.gameObject.SetActive(isAttacking);
-                }
-            }
-            else if (isAttacking)
-            {
-                BodyPart previousPart = (BodyPart)Enum.Parse(typeof(BodyPart), bodyPartDropdown.options[selectedBodyPartIndex].text);
-                combatSystem.opponent.HighlightBodyPart(previousPart, false);
-
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    selectedBodyPartIndex--;
-                    if (selectedBodyPartIndex < 0)
-                    {
-                        selectedBodyPartIndex = bodyPartDropdown.options.Count - 1;
-                    }
-                }
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    selectedBodyPartIndex++;
-                    if (selectedBodyPartIndex >= bodyPartDropdown.options.Count)
-                    {
-                        selectedBodyPartIndex = 0;
-                    }
-                }
-
-                BodyPart selectedPart = (BodyPart)Enum.Parse(typeof(BodyPart), bodyPartDropdown.options[selectedBodyPartIndex].text);
-                combatSystem.opponent.HighlightBodyPart(selectedPart, true);
-
-                bodyPartDropdown.value = selectedBodyPartIndex;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                if (isAttacking)
-                {
-                    OnAttackButton();
-                }
-                else
-                {
-                    OnDefendButton();
-                }
-            }
-        }
+        // Handle attack button click
     }
 
-
-    private void OnTurnChanged(bool isPlayerTurn)
+    private void OnDefendButtonClicked()
     {
-        attackButton.interactable = isPlayerTurn;
-        defendButton.interactable = isPlayerTurn;
-        if (isPlayerTurn)
-        {
-            isChoosingAction = true;
-            bodyPartDropdown.gameObject.SetActive(false);
-        }
-    }
-
-    private void PopulateBodyPartDropdown()
-    {
-        bodyPartDropdown.options.Clear();
-        foreach (BodyPart bodyPart in Enum.GetValues(typeof(BodyPart)))
-        {
-            Character characterToAttack = combatSystem.isPlayerTurn ? combatSystem.opponent : combatSystem.player;
-            if (!characterToAttack.IsBodyPartDestroyed(bodyPart))
-            {
-                bodyPartDropdown.options.Add(new TMP_Dropdown.OptionData() { text = bodyPart.ToString() });
-            }
-        }
-    }
-
-    private void OnAttackButton()
-    {
-        if (isChoosingAction)
-        {
-            isChoosingAction = false;
-            bodyPartDropdown.gameObject.SetActive(true);
-        }
-        else
-        {
-            BodyPart selectedPart = (BodyPart)Enum.Parse(typeof(BodyPart), bodyPartDropdown.options[bodyPartDropdown.value].text);
-            combatSystem.Attack(selectedPart);
-            isChoosingAction = true;
-            bodyPartDropdown.gameObject.SetActive(false);
-        }
-    }
-
-    
-    private void OnDefendButton()
-    {
-        if (isChoosingAction)
-        {
-            combatSystem.Defend();
-            isChoosingAction = true;
-        }
+        // Handle defend button click
     }
 }
